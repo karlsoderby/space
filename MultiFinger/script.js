af5a3f5986548b1662709952146b3322e0566404
Johan Hellgren <honajnahoj@gmail.com>
2018-11-01 12:54:14 +0100

    Months

--- MultiFinger/script.js
+++ MultiFinger/script.js
@@ -1,14 +1,19 @@
-
-//let pg = {movingUp: false, movingLeft: false, numGroups: 0, horizontalSpeed: 0, verticalSpeed: 0, absSpeed: 0, numPointers: 0, distance: 0, isApproaching: false};
 let newPos = 0;
 let pages = [];
-let pageSize = 300;
+let pageSize = 500;
 let currentScroll = 0;
 let currentPage = 0;
 let oldScroll = 0;
 let newScroll = 0;
 let destination = 0;
 let animating = false;
+let center = {x: 0, y: 0};
+let swipeDistance = 0;
+let lerpVal = 0;
+let numberOfFingers = 0;
+let months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+let years = ["2016", "2017", "2018"]
+let iterator = 0;
 
 function onDocumentReady() {
     let el = document.body;
@@ -29,9 +34,14 @@ function onDocumentReady() {
     el.addEventListener('gesturechange', e => e.preventDefault());
     el.addEventListener('gestureend', e => e.preventDefault());
 
-    for(let i = 0; i < 16; i++) {
-      pages.push(new page(i * pageSize));
+    /* for(let i = 0; i < 16; i++) {
+     
       //document.body.style.height = i * 50 + "px";
+    } */
+
+    while(months.length > 0) {
+      pages.push(new page(iterator * pageSize));
+      iterator ++;
     }
   
     setInterval(() => {
@@ -49,11 +59,23 @@ function onDocumentReady() {
     this.pageDiv.style.height = pageSize - 50 + "px";
     this.pageDiv.style.width = pageSize - 50 + "px";
     this.pageDiv.style.margin = "5%"
+    if(Math.random() > .3) {
+      this.month = document.createElement("H1");
+      this.month.style.position = "absolute";
+      this.month.style.left = this.yPos + "px";
+      this.month.style.top = pageSize + 100 + "px";
+      this.month.style.width = pageSize - 50 + "px";
+      this.month.style.textAlign = "center";
+      this.month.innerHTML = months[0];
+      months.shift();
+      this.pageDiv.appendChild(this.month);
+    }
     //this.pageDiv.style.left = "10%"
     this.pageDiv.style.backgroundColor = random_rgba();
    /*  for(let i = 0; i < 10; i++) {
       this.pageDiv.innerHTML += Math.random().toString(36).substring(7);
     } */
+    
     document.body.appendChild(this.pageDiv);
   }
 
@@ -72,19 +94,19 @@ Math.clamp = function(number, min, max) {
 
 function random_rgba() {
   var o = Math.round, r = Math.random, s = 255;
-  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
+  //r().toFixed(1)
 }
   
   function displayData () {
+    numberOfFingers = pointers.numOfPointers;
     oldScroll = currentScroll;
     let g = pointers.createPointerGroupings();
     let text = "";
     if (pointers.numOfPointers > 1) 
-      var { distance, isApproaching } = pointers.comparePointers(pointers.pointerIds[0], pointers.pointerIds[1]);      
-    
+      var { distance, isApproaching } = pointers.comparePointers(pointers.pointerIds[0], pointers.pointerIds[1]);
     if (g.numOfGroupings > 0) {
       let pg = g.groupings[0];
-      //pg = {movingUp: pg.isMovingUp, movingLeft: pg.isMovingLeft, numGroups: g.numOfGroupings, horizontalSpeed: pg.horizontalSpeed, verticalSpeed: pg.verticalSpeed, absSpeed: pg.speed, numPointers: pointers.numOfPointers, distance: distance, isApproaching: isApproaching};
       if(pointers.numOfPointers === 1) {
         if(pg.horizontalSpeed > 10 && !animating) {
           destination = 50;
@@ -96,40 +118,50 @@ function random_rgba() {
         }
       }
       if(pointers.numOfPointers  === 2) { 
-        
-       /*  currentScroll += (pg.verticalSpeed)
-        currentPage = Math.abs(map_range(currentScroll, 0, window.innerHeight, 0, 7)); 
-        currentScroll = Math.clamp(currentScroll, 0, document.body.style.height);
-        if(currentPage >= 0 && currentPage < pages.length) { 
-
-        } */
-        //console.log(pg.horizontalSpeed)
         if(pg.horizontalSpeed > 50 && !animating) {
           destination = pageSize;
           playAnimation(destination, 300);
+          center = {x: center.x + 500, y: 0};
         }
-        else if( pg.horizontalSpeed < - 50 && !animating) {
+        else if(pg.horizontalSpeed < - 50 && !animating) {
           destination = -pageSize;
           playAnimation(destination, 300);
+          center = {x: center.x - 500, y: 0};
         }
       }
     } 
-    /* for(page of pages) {
-      //page.pageDiv.style.left = lerp(parseFloat(page.pageDiv.style.left), parseFloat(page.pageDiv.style.left) + destination, 0.05) + "px";
-      page.pageDiv.style.left = parseFloat(page.pageDiv.style.left) + destination + "px";
-      if(Math.abs(parseFloat(page.pageDiv.style.left) - (parseFloat(page.pageDiv.style.left) + destination)) > 100) {
-        destination = 0;
+    /*  for(page of pages) {
+      if(Math.abs(parseFloat(page.pageDiv.style.left) - (parseFloat(page.pageDiv.style.left) + center.x)) > 10) {
+        animating = true;
+        lerpVal += 0.01;
+      } 
+      else {
+        animating = false;
+        lerpVal = 0;
+      } 
+      console.log(lerpVal)
+      if(animating) {
+        page.pageDiv.style.left = lerp(parseFloat(page.pageDiv.style.left), parseFloat(page.pageDiv.style.left) + center.x, lerpVal) + "px";
+        if(lerpVal >= 1) {
+          lerpVal = 0;
+          animating = false;
+        }
       }
+     
+      //console.log(page.pageDiv.style.left)
     } */
   }
 
   function playAnimation(distance, time) {
     animating = true;
-    console.log("animating")
     for(page of pages) {
+      let centerX = parseFloat(page.pageDiv.style.left) - (pageSize / 2);
+      let newSize = map_range(Math.abs(window.innerWidth - parseFloat(page.pageDiv.style.left)), 0, 7000, 0, 1);
       const keyframes = [
         {transform: "translateX(0px)"},
-        {transform: "translateX(" + distance + "px)"}
+        {transform: "translateX(" + distance + "px)"},
+        /* {transform: "scale(1)"},
+        {transform: "scale(" + newSize + ")"} */
       ];
       let anim = page.pageDiv.animate(keyframes, {
         iterations: 1,
@@ -139,13 +171,17 @@ function random_rgba() {
       }) 
       page.pageDiv.style.left = (parseFloat(page.pageDiv.style.left) + distance) + "px";
       anim.onfinish = () => {
-        animating = false;
+        if(animating) animating = false;
       }
     }
-       
   }
 
   function onPointerUp(e) {
+    console.log(numberOfFingers)
+    if(numberOfFingers == 2) {
+      
+    }
+    numberOfFingers = 0;
     pointers.removePointer(e.pointerId);
     let el = getOrCreate(e);
     el.classList.remove('down');
@@ -157,6 +193,7 @@ function random_rgba() {
   }
   
   function onPointerLeave(e) {
+    //console.log(numberOfFingers)
     pointers.removePointer(e.pointerId);
     e.preventDefault();
     let el = getOrCreate(e);
@@ -187,8 +224,6 @@ function random_rgba() {
     el.addEventListener('gesturestart', e => e.preventDefault());
     el.addEventListener('gesturechange', e => e.preventDefault());
     el.addEventListener('gestureend', e => e.preventDefault());
-  
-
     el.id = id;
     document.body.appendChild(el);
     return el;
